3cb694aca5832f482fef77597eb49a42299b2ff2
David Mostoller <dmostoller@gmail.com>
2024-03-12 17:32:26 -0400

    initial project setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import './semantic/dist/semantic.css'
+
+
+function App() {
+  return <h1>Project Client</h1>;
+}
+
+export default App;
