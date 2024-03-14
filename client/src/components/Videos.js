b22a8356575246ca09b2cbcdf9095e2c49791dd2
David Mostoller <dmostoller@gmail.com>
2024-03-14 19:51:39 -0400

    video playlist embed

--- client/src/components/Videos.js
+++ client/src/components/Videos.js
@@ -1,11 +1,12 @@
 
-const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
+import React  from "react"
+// const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
-export async function getServerSideProps() {
-    const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLkiLSmC1caWvoUoGTKuBhbS&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`);
-    const data = await res.json();
-    return data 
-    }
+// export async function getServerSideProps() {
+//     const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&playlistId=PLkiLSmC1caWvoUoGTKuBhbS&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`);
+//     const data = await res.json();
+//     return data 
+//     }
 
 
 function Videos() {
@@ -15,6 +16,7 @@ function Videos() {
     return (
         
         <>
+        <div style='padding:56.25% 0 0 0;position:relative;'><iframe src='https://vimeo.com/showcase/11038900/embed' allowfullscreen frameborder='0' style='position:absolute;top:0;left:0;width:100%;height:100%;'></iframe></div>
         </>
 
     )
