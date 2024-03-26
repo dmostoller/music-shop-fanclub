a49c23d5bf1c767bb8ae52ac26786c2ddcf00732
David Mostoller <dmostoller@gmail.com>
2024-03-26 19:23:52 -0400

    kjd

--- server/config.py
+++ server/config.py
@@ -31,12 +31,12 @@ app.json.compact = False
 app.secret_key = os.environ.get("SECRET_KEY")
 
 # Define metadata, instantiate db
-metadata = MetaData(
-    naming_convention={
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    }
-)
-db = SQLAlchemy(metadata=metadata)
+# metadata = MetaData(
+#     naming_convention={
+#         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+#     }
+# )
+db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
 
